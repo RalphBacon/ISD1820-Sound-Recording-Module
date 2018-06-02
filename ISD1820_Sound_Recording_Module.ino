068746cfced76f916c04ca830fb04b20be2e78f1
Ralph Bacon <arduino@jargongeneration.uk>
2018-06-02 20:21:41 +0100

    Arduino sketch just to demonstrate the module

---
+++ ISD1820_Sound_Recording_Module.ino
@@ -0,0 +1,29 @@
+/*
+ * Simple circuit to test the triggering of the IDS1820 Voice Recording Module
+ * 
+ * Connect VCC to the 3.3V of the Arduino Uno/Nano etc
+ * Connect GND to a spare GND pin on your Arduino
+ * Connect P-E (pulse edge) pin of the IDS1820 to pin 2 of the Arduino
+ * Ensure you have removed the jumper from P-E on the module
+ */
+
+#define triggerPin 2
+
+void setup() {
+
+  Serial.begin(9600);
+
+  // Set the trigger pin in this sequence to prevent false triggering on start
+  digitalWrite(triggerPin, LOW);
+  pinMode(triggerPin, OUTPUT);  
+  
+  Serial.println("Start up completed");
+}
+
+void loop() {
+
+  digitalWrite(triggerPin, HIGH);
+  delay(100);
+  digitalWrite(triggerPin, LOW);
+  delay(10000);  
+}
